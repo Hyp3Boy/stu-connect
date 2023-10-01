d28a3e134de8f32984bf8c40e5b23cb9f8e5c496
Lenin Chavez <lenin.chavez@utec.edu.pe>
2023-10-01 01:03:04 -0500

    Added setup.sh
    Setup.sh setup the venv :)

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Nombre del entorno virtual
+venv_name="venv"
+
+# Comando para crear el entorno virtual
+python3 -m venv "$venv_name"
+
+# Activar el entorno virtual
+source "$venv_name/bin/activate"
+
+# Instalar paquetes con pip
+pip3 install flask flask_bcrypt flask_session flask_sqlalchemy redis
+
+# Mostrar mensaje de finalización
+echo "Entorno virtual '$venv_name' creado y paquetes instalados."
+
+# Desactivar el entorno virtual (opcional)
+# deactivate
+
